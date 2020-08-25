61e20f8a0315290c784c629ed46424411b312bd5
Tirumala009 <69470807+Tirumala009@users.noreply.github.com>
2020-08-25 17:39:31 +0100

    Updates

---
+++ precache-manifest.e91d7b8fb814996a0266731fa4ff25b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2e593a0c828ad80583ab356fc0cc078",
+    "url": "/ticketmaster/index.html"
+  },
+  {
+    "revision": "9566180e7a7a3e9c3d70",
+    "url": "/ticketmaster/static/css/main.03b46c5c.chunk.css"
+  },
+  {
+    "revision": "6e2f93531487de9da4f6",
+    "url": "/ticketmaster/static/js/2.fb1451f1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ticketmaster/static/js/2.fb1451f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9566180e7a7a3e9c3d70",
+    "url": "/ticketmaster/static/js/main.95ddb7e5.chunk.js"
+  },
+  {
+    "revision": "278c30361deffef4bb5a",
+    "url": "/ticketmaster/static/js/runtime-main.274b6cf6.js"
+  }
+]);
